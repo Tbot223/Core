067fcd91c7a882c4fda3d0a645d4e3e9cb28934c
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2025-12-04 11:05:26 +0900

    Update Result.py

--- tbot223_core/Result.py
+++ tbot223_core/Result.py
@@ -3,11 +3,6 @@ from typing import Any, NamedTuple, Optional
 
 #internal Modules
 
-"""
-Do not add any functionality here for now. This file exists to differentiate between Core and CoreV2 modules.
-
-Avoid using ExtendedResult or ResultUtils in CoreV2 for now, as dataclass features or conversion utilities are not currently needed.
-"""
 class Result(NamedTuple):
     """
     Class representing operation results in CoreV2
